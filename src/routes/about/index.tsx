480f70c5ca24b06bdb56aefef961dfa424700ba3
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2025-02-11 17:28:31 +0200

    Fix Ts issues and add toml file

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -2,34 +2,42 @@ import { createFileRoute } from '@tanstack/react-router';
 import { useQuery } from '@tanstack/react-query';
 import supabase from '../../config/supabase/supabase';
 
+interface ExpTableRow {
+  created_at: string;
+  name: string;
+  ball: boolean;
+  comment: string;
+}
+
 export const Route = createFileRoute('/about/')({
   component: RouteComponent,
 });
 
-const fetchExpTable = async () => {
+const fetchExpTable = async (): Promise<ExpTableRow[]> => {
   const { data, error } = await supabase
     .from('expTable')
-    .select('')
+    .select('*')
 
   if (error) {
     throw new Error(error.message)
   }
-  return data
-}
+  return data as ExpTableRow[]
+};
 
 function RouteComponent() {
-  const { data, error, isLoading } = useQuery({
+  const { data, error, isLoading } = useQuery<ExpTableRow[]>({
     queryKey: ['expTable'],
     queryFn: fetchExpTable,
   });
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Error: {error.message}</div>
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return null;
 
   return (
     <div>
       About:
       {data[0]?.comment}
     </div>
-  )
+  );
 }
